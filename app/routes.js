82290f9d8a40a4bba683ce2591289976a70a3db5
CrugBarat <craigtbarratt@gmail.com>
2020-07-08 22:59:44 +0100

    adds loose validation to persist email to success page
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- app/routes.js
+++ app/routes.js
@@ -116,7 +116,7 @@ router.get('/forms/govuk-forms/universal-credit/UCcreateAccount', function (req,
   var select_q1 = req.session.data ['question1']
   var select_q2 = req.session.data ['question2']
   var password_match = (req.session.data ['password2'] === create_password)
-  var uc_error_create_account = ( // receive the value of all the potential errors on that page 
+  var uc_error_create_account = ( // receive the value of all the potential errors on that page
     create_username === '' ||
     create_password === '' ||
     create_answwer1 === '' ||
@@ -142,7 +142,7 @@ router.post('/forms/govuk-forms/universal-credit/UCcreateAccount', function (req
   var select_q1 = req.session.data ['question1']
   var select_q2 = req.session.data ['question2']
   var password_match = (req.session.data ['password2'] === create_password)
-  var uc_error_create_account = ( // receive the value of all the potential errors on that page 
+  var uc_error_create_account = ( // receive the value of all the potential errors on that page
     create_username === '' ||
     create_password === '' ||
     create_answwer1 === '' ||
@@ -157,13 +157,13 @@ router.post('/forms/govuk-forms/universal-credit/UCcreateAccount', function (req
     return res.redirect('/forms/govuk-forms/universal-credit/UCcreateAccount')
   } else {
     return res.redirect('/forms/govuk-forms/universal-credit/UCaboutYou')
-  } 
+  }
 })
 
 router.post('/forms/govuk-forms/universal-credit/UCforgotUsername', function (req, res) {
   var emailAddress = req.session.data['email-to-resend-username']
   var regExpEmail = /^[a-zA-Z0-9=*!$&_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-  if (emailAddress === '' || !regExpEmail.test(emailAddress)) 
+  if (emailAddress === '' || !regExpEmail.test(emailAddress))
   {
     return res.redirect('/forms/govuk-forms/universal-credit/UCforgotUsernameError')
   }
@@ -173,7 +173,7 @@ router.post('/forms/govuk-forms/universal-credit/UCforgotUsername', function (re
 router.post('/forms/govuk-forms/universal-credit/UCforgotUsernameError', function (req, res) {
   var emailAddress = req.session.data['email-to-resend-username']
   var regExpEmail = /^[a-zA-Z0-9=*!$&_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-  if (emailAddress === '' || !regExpEmail.test(emailAddress)) 
+  if (emailAddress === '' || !regExpEmail.test(emailAddress))
   {
     return res.redirect('/forms/govuk-forms/universal-credit/UCforgotUsernameError')
   }
@@ -188,7 +188,7 @@ router.post('/forms/govuk-forms/universal-credit/UCsignIn', function (req, res) 
     return res.redirect('/forms/govuk-forms/universal-credit/UCsignedIn')
   } else {
     return res.redirect('/forms/govuk-forms/universal-credit/UCsignIn')
-  } 
+  }
 })
 
 // this is just so the value of the email address if entered can be stored and re-used on the next page
@@ -279,7 +279,7 @@ router.get('/forms/erc-forms/council-tax-online/pay-council-tax/PCTpaymentEntry'
   var used_cancel = req.session.data ['usedcancel']
   if (used_cancel) {
     // delete all session data
-    req.session.data = {} 
+    req.session.data = {}
   }
    res.render('forms/erc-forms/council-tax-online/pay-council-tax/PCTpaymentEntry')
 })
@@ -324,44 +324,44 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLdeclaration2')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLrep-info', function (req, res) {
     var repInfo = req.session.data['permission']
-    if (repInfo && repInfo.includes('agreed') 
+    if (repInfo && repInfo.includes('agreed')
   )  {
       return res.redirect('/forms/erc-forms/housing/CBLdeclaration2')
     }
       res.redirect('/forms/erc-forms/housing/CBLrep-info-error')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLrep-info-error', function (req, res) {
     var repInfo = req.session.data['permission']
-    if (repInfo && repInfo.includes('agreed') 
+    if (repInfo && repInfo.includes('agreed')
   )  {
       return res.redirect('/forms/erc-forms/housing/CBLdeclaration2')
     }
       res.redirect('/forms/erc-forms/housing/CBLrep-info-error')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLdeclaration2', function (req, res) {
     var declaration = req.session.data['declaration2']
-    if (declaration && declaration.includes('has-read') 
+    if (declaration && declaration.includes('has-read')
   )  {
       return res.redirect('/forms/erc-forms/housing/CBL16')
     }
       res.redirect('/forms/erc-forms/housing/CBLdeclaration2-error')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLdeclaration2-error', function (req, res) {
     var declaration = req.session.data['declaration2']
-    if (declaration && declaration.includes('has-read') 
+    if (declaration && declaration.includes('has-read')
   )  {
       return res.redirect('/forms/erc-forms/housing/CBL16')
     }
       res.redirect('/forms/erc-forms/housing/CBLdeclaration2-error')
   })
-  
-  
+
+
   router.post('/forms/erc-forms/housing/CBL16', function (req, res) {
     var is16 = req.session.data['is-16']
     if (is16 === 'no') {
@@ -369,7 +369,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLimmigration')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLimmigration', function (req, res) {
     var immigrationControl = req.session.data['immigration']
     if (immigrationControl === 'no') {
@@ -377,7 +377,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLrecourse')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLrecourse', function (req, res) {
     var isrecourse = req.session.data['is-recourse']
     if (isrecourse === 'yes') {
@@ -385,7 +385,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLhomeless-question')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLhomeless-question', function (req, res) {
     var homeless = req.session.data['homeless']
     if (homeless=== 'yes') {
@@ -393,36 +393,36 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLaddress')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLaddress', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLaboutyou')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLaboutyou', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLcontact')
   })
-  
-  
+
+
   router.post('/forms/erc-forms/housing/CBLcontact', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLdob')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLcontact1', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLdob')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLdob', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLgender')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLgender', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLsummary-aboutyou')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLwho-moving', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLliving-alone')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLliving-alone', function (req, res) {
     var livingAlone = req.session.data['living-alone']
     var gender = req.session.data['gender-applicant']
@@ -434,8 +434,8 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLinfo-housed')
   })
-  
-  
+
+
   router.post('/forms/erc-forms/housing/CBLinfo-housed2', function (req, res) {
     var needAdd = req.session.data['need-add']
     if (needAdd ==='no') {
@@ -443,8 +443,8 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     return res.redirect('/forms/erc-forms/housing/CBLadd-person1-loop')
   })
-  
-  
+
+
   router.post('/forms/erc-forms/housing/CBLadd-person1-loop', function (req, res) {
     var addPerson2 = req.session.data['add-person2']
     if (addPerson2 === 'yes') {
@@ -452,7 +452,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLbabies')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLadd-person2-loop', function (req, res) {
     var addPerson3 = req.session.data['add-person3']
     if (addPerson3 === 'yes') {
@@ -460,11 +460,11 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLbabies')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLadd-person3-loop', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLbabies')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLbabies', function (req, res) {
     var livingAlone = req.session.data['living-alone']
     if (livingAlone === 'yes') {
@@ -472,12 +472,12 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
   }
     res.redirect('/forms/erc-forms/housing/CBLjoint')
   })
-  
-  
+
+
   router.post('/forms/erc-forms/housing/CBLjoint', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLother-children')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLother-children', function (req, res) {
     var otherChidren = req.session.data['other-children']
     if (otherChidren === 'yes') {
@@ -485,7 +485,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLcurrent-address')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLadd-child1-loop', function (req, res) {
     var addPerson2 = req.session.data['add-child2']
     if (addPerson2 === 'yes') {
@@ -493,11 +493,11 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLcurrent-address')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLadd-child2-loop', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLcurrent-address')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBL5yearsadd-question', function (req, res) {
     var addAddress = req.session.data['add-address']
     var hasJoint = req.session.data['is-joint']
@@ -509,7 +509,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLlegal')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBL5yearsadd', function (req, res) {
     var addAddress2 = req.session.data['add-address2']
     var hasJoint = req.session.data['is-joint']
@@ -521,7 +521,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLlegal')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLaddress2-loop', function (req, res) {
     var hasJoint = req.session.data['is-joint']
     if (hasJoint === 'yes'){
@@ -529,7 +529,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLlegal')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLjoint-info', function (req, res) {
     var sameAddresses = req.session.data['same-addresses']
     if (sameAddresses=== 'yes') {
@@ -537,7 +537,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBL5yearsadd-joint')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBL5yearsadd-joint', function (req, res) {
     var addAddress2Joint = req.session.data['add-address2-joint']
     if (addAddress2Joint === 'yes') {
@@ -545,11 +545,11 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLlegal')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLaddress2-loop-joint', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLlegal')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLbedroom', function (req, res) {
     var notMoving = req.session.data['who-not-moving']
     if (notMoving === 'yes') {
@@ -557,7 +557,7 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLreasons')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLdeclaration1', function (req, res) {
     var know = req.session.data['know-erc']
     if (know === 'yes') {
@@ -565,8 +565,12 @@ router.post('/forms/erc-forms/housing/CBLapplicant', function (req, res) {
     }
     res.redirect('/forms/erc-forms/housing/CBLextra-reason-abuse')
   })
-  
+
   router.post('/forms/erc-forms/housing/CBLextra-reason-medical', function (req, res) {
     res.redirect('/forms/erc-forms/housing/CBLend')
   })
-  
+
+  router.post('/forms/other-forms/patient-access/pa-register2', function (req, res) {
+    const email = req.session.data['email']
+    res.redirect('/forms/other-forms/patient-access/pa-success')
+  })
